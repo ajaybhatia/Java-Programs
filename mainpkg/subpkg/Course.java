f4b5a13d3ac2de740d7b9e93214a31819eb8d1e8
ajaybhatia <prof.ajaybhatia@gmail.com>
2015-09-09 14:31:44 +0530

    Packages

---
+++ mainpkg/subpkg/Course.java
@@ -0,0 +1,68 @@
+/*
+ * Course.java
+ * 
+ * Copyright 2015 Ajay Bhatia <prof.ajaybhatia@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+package mainpkg.subpkg;
+
+public class Course {
+	private int id;
+	private String name;
+	private int duration;
+	
+	public Course() {}
+	
+	public Course(int id, String name, int duration) {
+		this.id = id;
+		this.name = name;
+		this.duration = duration;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	
+	public int getDuration() {
+		return duration;
+	}
+	
+	@Override // Annotation
+	public String toString() {
+		return "(" + id + ") " + name + " - " + duration + " year(s).";
+	}
+}
+
